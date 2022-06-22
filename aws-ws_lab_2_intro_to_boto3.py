a6a672c183082c7808cf39f12e6ff49bef2c5a21
Marquise <mardanmw27+LUIT@gmail.com>
2022-06-22 14:40:58 +0000

    AWS lab 2 initial commit

---
+++ aws-ws_lab_2_intro_to_boto3.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env Python3
+
+import boto3
+
+client = boto3.client('translate')
+
+def translate_text(): 
+    response = client.translate_text(
+        Text='I am learning to code in AWS', # Assigning the value of the string to the variable Text
+        SourceLanguageCode='en', 
+        TargetLanguageCode='es' 
+    )
+#### Add the new text below this line ####
+    print(response) # this code is inside the function and will print the contents of the variable 'response' 
+
+translate_text() # This line will call our function. Without it, python will not do anything.

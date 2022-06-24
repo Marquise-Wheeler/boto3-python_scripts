0930ecc840e89fd695603f2677ffe9e67a3b3620
Marquise Wheeler <mardanmw27+LUIT@gmail.com>
2022-06-24 12:23:50 +0000

    initial commit

---
+++ count_buckets.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env Python3
+
+import boto3
+resource=boto3.resource("s3")
+bucket_list=list(resource.buckets.all())
+print(len(bucket_list))
+
+for bucket in resource.buckets.all():
+    print(bucket.name)
+    
+    

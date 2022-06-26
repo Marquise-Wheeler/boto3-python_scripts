d4317ce7e184d8f262119ea592b0f3f8de12af16
Marquise Wheeler <mardanmw27+LUIT@gmail.com>
2022-06-26 13:44:56 +0000

    Addint delete multiple to script

--- delete_object.py
+++ delete_object.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env Python3
 
+#import boto3
+
+
+#s3_resource=boto3.client("s3")
+# delete single object
+#s3_resource.delete_object(Bucket='techrescue',
+#    Key='Give-a-man-fish.png')
+
+###################################    
+#delete multiple objects
 import boto3
+import os
+import glob
 
 s3_resource=boto3.client("s3")
-s3_resource.delete_object(Bucket='techrescue',
-    Key='Give-a-man-fish.png')
+ 
+objects=s3_resource.list_objects(Bucket="techrescue")["Contents"]
+print(len(objects))

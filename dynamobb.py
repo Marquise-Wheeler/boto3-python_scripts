db270ee6d394428d8865cb0930099879ccbcb759
Marquise Wheeler <107084312+Marquise-Wheeler@users.noreply.github.com>
2022-06-27 07:46:04 -0500

    Update dynamobb.py
    Removed notes from bottom of file.

--- dynamobb.py
+++ dynamobb.py
@@ -32,11 +32,3 @@ table = dynamodb.create_table (
     )
 print(table)
     
-# BASIC
-
-# ADVANCED Use boto3 and Python to query a table, remove an item from a table, create a table, and delete a table.
-# COMPLEX 
-#1) Create a lambda function using boto3 and Python to query a table, return an item from a table and remove/delete an item from a table.
-#2) Run a test of the lambda function to verify you were able to do all of the previous actions.
-#3) Create APIs using API Gateway using the console that will each return query a table, return an item, delete an item by calling your lambda function.
-#Note: You can reference the following documentation to point you in the right direction.
